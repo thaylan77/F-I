12e8b8a9f8519e04a6a22674ef6d229c42f65754
Thaylã Cavalcante <thaylancavalcante@gmail.com>
2023-04-18 16:42:12 -0300

    Initial commit

---
+++ Controllers/Base.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TerraF.Controllers
+{
+    public class Base : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
